1e9388b6414251ef89aa693f383df966517c4961
delchev <delchevn@gmail.com>
2024-12-25 14:10:00 +0200

    formatting

--- examples/accordion/app.js
+++ examples/accordion/app.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
                 { value: 'item-1', title: 'Is it accessible?', content: 'Yes. It adheres to the WAI-ARIA design pattern.' },
                 { value: 'item-2', title: 'Is it unstyled?', content: 'Yes. It\'s unstyled by default, giving you freedom over the look and feel.' },
                 { value: 'item-3', title: 'Can it be animated?', content: 'Yes! You can use the transition prop to configure the animation.' },
-              ],
+            ],
             defaultValue: 'item-1',
         };
     }
